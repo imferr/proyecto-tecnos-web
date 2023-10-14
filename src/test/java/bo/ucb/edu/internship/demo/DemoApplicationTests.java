e8c645c4f5eadf568ac56543f3b402bb8655c936
imferr <117535778+imferr@users.noreply.github.com>
2023-10-14 16:57:49 -0400

    creando dependencias

---
+++ src/test/java/bo/ucb/edu/internship/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.ucb.edu.internship.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
